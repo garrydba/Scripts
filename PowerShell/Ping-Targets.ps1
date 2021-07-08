15ac6b70c9f1daed6a57509238bd5cb8c90b8916
GP <greatg@MyBook.local>
2021-07-08 17:52:55 -0400

    New PS script to continuously check availability of IP (or collection of IP)

---
+++ PowerShell/Ping-Targets.ps1
@@ -0,0 +1,25 @@
+Clear-Host
+
+<#
+    Purpose: Continuously check availability of IP (or collection of IP)
+    How it works: This script takes list of IP(s) specified in file called ""IP_List.txt". 
+                  Then goes through each IP address. And use Test-Connection cmdlet to check availability of IP. In theory this cmdlet works same as PING.
+                  If device fails to respond, script logs IP and timestamp in a file called PING.txt.
+#>
+
+$scriptPath = $MyInvocation.MyCommand.Path
+$setDir = Split-Path $scriptPath -Parent
+Set-Location $setDir
+$ipList = Get-Content IP_List.txt
+do {
+    foreach ($ip in $ipList) {
+        if (! (Test-Connection -ComputerName $ip -Count 1 -Quiet)) {
+            $dt = Get-Date    
+            $OutPut = "$ip down : " + $dt
+            $OutPut  | out-file "Ping.log" -append
+        }
+    }
+
+    start-sleep -s 1
+}
+while ("THIS" -ne "THAT")
